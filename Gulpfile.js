93867f041e024c0ab82c75c9eb42075bb47437f1
EliverLara <eliverlara@gmail.com>
2020-09-18 13:55:19 -0500

    Add cinnamon DE support

--- Gulpfile.js
+++ Gulpfile.js
@@ -18,6 +18,16 @@ gulp.task('shell-style', function(done) {
     done();
 });
 
+gulp.task('cinnamon-style', function(done) {
+    gulp.src('cinnamon/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./cinnamon/'))
+        .pipe(exec(' gsettings set org.cinnamon.desktop.interface gtk-theme "Dracula"'))
+        .pipe(exec(' gsettings set org.cinnamon.desktop.wm.preferences theme "Dracula"'))
+        .pipe(exec(' gsettings set org.cinnamon.theme name "Dracula"'))
+    done();
+});
+
 //Watch task
 gulp.task('default',function() {
     gulp.watch('gtk-3.20/**/*.scss', gulp.series('styles'));
@@ -25,4 +35,8 @@ gulp.task('default',function() {
 
 gulp.task('shell',function() {
     gulp.watch('gnome-shell/*.scss', gulp.series('shell-style'));
+});
+
+gulp.task('cinnamon',function() {
+    gulp.watch('cinnamon/**/*.scss', gulp.series('cinnamon-style'));
 });
